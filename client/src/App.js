54eae1223f96cc107fe178ba780dc992b4c91eb5
Lamia Hossain <lamiahossain2017@gmail.com>
2024-05-29 01:03:28 +0600

    1. Routing enabled 2. Installed Axios and DaisyUI plugin 3. Application name changed

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import LandingPage from "./landing/LandingPage";
+import Timeline from "./timeline/Timeline";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-[#eef0f1] dark:bg-[#333334] min-h-screen">
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/timeline" element={<Timeline />} />
+      </Routes>
     </div>
   );
 }
